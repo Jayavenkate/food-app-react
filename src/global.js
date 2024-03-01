e57a3a76ee59d3f69eb418da76397c03b2107639
Jayavenkate <jayajaikithuja@gmail.com>
2024-03-01 16:18:10 +0300

    app

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-// export const API = "http://localhost:7000";
-export const API = "https://capstone-node.vercel.app";
+export const API = "http://localhost:7000";
+// export const API = "https://capstone-node.vercel.app";
